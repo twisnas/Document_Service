362aa8fdcf4c603bcadc91b8e544ae720a27bccc
Travis Wisnasky <twisnas@users.noreply.github.com>
2017-09-15 17:42:01 -0500

    initial checkin

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,14 @@
+package hello;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String printHello(){
+        return "Hello World";
+    }
+}
---
+++ src/main/java/hello/StartingPoint.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartingPoint {
+
+    public static void main(String[] args){
+        SpringApplication.run(StartingPoint.class, args);
+
+    }
+}
